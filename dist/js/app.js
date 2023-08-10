74710291ca19fee37ce7360862e93099e88dc34c
livehass <silva.felipe12@hotmail.com>
2023-08-10 18:45:28 -0300

    new import

---
+++ dist/js/app.js
@@ -0,0 +1,1 @@
+import { negociacao } from "./models/negociacao";
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,5 +1,12 @@
-class negociacao {
+export class negociacao {
     #data;
     #quantidade;
     #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
 }
+
