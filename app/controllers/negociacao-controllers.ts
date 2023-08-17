26181e8616484053851f19fb611e894b88af1077
livehass <silva.felipe12@hotmail.com>
2023-08-17 20:56:48 -0300

    novo metodo adiciona

--- app/controllers/negociacao-controllers.ts
+++ app/controllers/negociacao-controllers.ts
@@ -1,5 +1,21 @@
-export class NegociacaoCOntroller {
+export class NegociacaoController {
     private inputData;
     private inputQuantidade;
     private inputValor;
+
+    constructor() {
+        this.inputData = document.getElementById("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+
+    }
+    adiciona() {
+        console.table(this.inputData);
+        console.table(this.inputQuantidade);
+        console.table(this.inputValor);
+
+    }
 }
+
+
+
--- dist/js/controllers/negociacao-controllers.js
+++ dist/js/controllers/negociacao-controllers.js
@@ -1,2 +1,12 @@
-export class NegociacaoCOntroller {
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.getElementById("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        console.table(this.inputData);
+        console.table(this.inputQuantidade);
+        console.table(this.inputValor);
+    }
 }
